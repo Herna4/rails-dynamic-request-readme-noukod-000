779d8c8bd42d9fd4dc07742b8f0a9ec6958c955c
Herna Michel <labellehernamichel@yahoo.fr>
2019-06-26 07:11:23 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,7 @@
-class StudentsController < ApplicationController
-   def index
-     @students = Student.all
-   end
+class PostsController < ApplicationController
 
-   def show
-     @student = Student.find(params[:id])
-   end
+    def show
+        @post = Post.find(params[:id])
+    end
+    
 end
